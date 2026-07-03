45681ed94032ea8267ad4569cbfcb5b1e5563ad9
Davi Fernandes <davi.fer159@gmail.com>
2026-07-03 14:00:22 -0300

    feat: add ContainweWrapper in barbershop search page

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,4 +1,5 @@
 import BarbeshopItem from "../components/barbershop-item";
+import ContainerWrapper from "../components/container";
 import Header from "../components/header";
 import Search from "../components/search";
 import { getBarbershops } from "../data/get-barbershops";
@@ -15,20 +16,22 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   return (
     <div>
       <Header />
-      <div className="px-5">
-        <div className="mt-6">
-          <Search />
+      <ContainerWrapper>
+        <div className="px-5">
+          <div className="mt-6">
+            <Search />
+          </div>
+          <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
+            Resultados para &quot;{searchParams?.title || searchParams?.service}
+            &quot;
+          </h2>
+          <div className="grid grid-cols-2 gap-4">
+            {barbershops.map((barbershop) => (
+              <BarbeshopItem barbershop={barbershop} key={barbershop.id} />
+            ))}
+          </div>
         </div>
-        <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
-          Resultados para &quot;{searchParams?.title || searchParams?.service}
-          &quot;
-        </h2>
-        <div className="grid grid-cols-2 gap-4">
-          {barbershops.map((barbershop) => (
-            <BarbeshopItem barbershop={barbershop} key={barbershop.id} />
-          ))}
-        </div>
-      </div>
+      </ContainerWrapper>
     </div>
   );
 };
